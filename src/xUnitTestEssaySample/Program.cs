d02807c7170dbbc28cbd8a5bd1b5e8b5de99aa6f
Flávio Borges Pinto <flavio.borges@cedrotech.com>
2018-05-24 10:19:09 -0300

    Initial commit

---
+++ src/xUnitTestEssaySample/Program.cs
@@ -0,0 +1,38 @@
+﻿ using System;
+
+namespace xUnitTestEssaySample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int parcela1 = Int32.Parse(Console.ReadLine());
+                int parcela2 = Int32.Parse(Console.ReadLine());
+                int total = Soma(parcela1, parcela2);
+
+                Console.WriteLine("Resultado: " + total.ToString());
+                Console.ReadKey();
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void RetornarExcecao()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static int Soma(int parcela1, int parcela2)
+        {
+            int resultado = parcela1 + parcela2;
+            return resultado;
+        }
+
+    }
+}
---
+++ src/xUnitTestEssaySampleTests/Tests.cs
@@ -0,0 +1,6 @@
+﻿namespace xUnitTestEssaySampleTests
+{
+    class Tests
+    {
+    }
+}
